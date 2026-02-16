43b7748c791b0dfd7bed242ed472cfad8f7de105
Shivansh-Rai <shivanshrai2316@gmail.com>
2026-02-16 23:22:54 +0530

    nil

---
+++ day1/datatypes.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n1; //4 bytes most common
+    long long n2; //large values
+    // use long long when constraints are up to 10^10 or more.
+    double n3; //decimals
+    return 0;
+}
---
+++ day1/forwhileloop.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n=5;
+    for (int i=0;i<n;i++){
+        cout<<i<<endl;
+    }
+    int i=0;
+    while (i<=n){
+        cout<<i<<endl;
+        i=i+1;
+    }
+    return 0;
+}
+ 
